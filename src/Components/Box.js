1da80ebfdb832bf76d40ffd70341dcff8833f69e
Ayushi <rayushi142@gmail.com>
2022-10-07 02:26:49 +0530

    Added Dark Mode Toggle

---
+++ src/Components/Box.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const BoxStyle = styled.div`
+   display: flex;
+    margin: 24px;
+`;
+
+export const Box = ({ children }) => (
+  <BoxStyle>
+    <div className="box">{children}</div>
+  </BoxStyle>
+);
---
+++ src/Components/theme.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+const lightTheme = {
+  bgColor: "white",
+  textColor: "black"
+};
+
+const darkTheme = {
+  bgColor: "black",
+  textColor: "green"
+};
+
+const themeKey = "theme";
+const ThemeMode = {
+  Dark: "dark",
+  Light: "light"
+};
+
+const isDarkModeEnabled = () =>
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+const setDarkModeCachedValue = (value) =>
+  window.localStorage.setItem(
+    themeKey,
+    value ? ThemeMode.Dark : ThemeMode.Light
+  );
+const getTheme = (darkMode) => (darkMode ? darkTheme : lightTheme);
+const getDarkModeCachedValue = () => {
+  const state = window.localStorage.getItem(themeKey);
+  if (state !== null) {
+    return state === ThemeMode.Dark;
+  }
+  return state;
+};
+
+export function useDarkMode() {
+  const [darkMode, setDarkMode] = useState(
+    getDarkModeCachedValue() ?? isDarkModeEnabled()
+  );
+  const [theme, setTheme] = useState(getTheme(darkMode));
+
+  useEffect(() => {
+    setDarkModeCachedValue(darkMode);
+    setTheme(getTheme(darkMode));
+  }, [darkMode]);
+
+  return { darkMode, setDarkMode, theme };
+}
