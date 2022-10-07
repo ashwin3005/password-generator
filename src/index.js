609ac2a42e237d9ec909320b622347d87392e37c
Kanishq Kancharla <Kanishq0106@gmail.com>
2022-10-06 23:12:16 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Textarea from "./Components/Textarea.jsx";
+import Textarea from "./Components/Textarea.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
